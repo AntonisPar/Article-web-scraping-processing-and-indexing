1add63862d71a368095eaf356a1555bfc5784d1f
antonis <antonis@antonis.com>
2021-12-08 15:30:06 +0200

    progress

--- meros2attempt3.py
+++ meros2attempt3.py
@@ -4,23 +4,42 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import names
 import numpy as np
 import nltk
+import pandas as pd
+import os
 nltk.download('names')
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+path = "/home/antonis/Downloads/20news-bydate/20news-bydate-train"
 
-groups = fetch_20newsgroups()
-
-data_train = fetch_20newsgroups(subset='train', random_state=21)
-train_label = data_train.target
-data_test = fetch_20newsgroups(subset='test', random_state=21)
-test_label = data_test.target
-len(data_train.data), len(data_test.data), len(test_label)
-
-np.unique(test_label)
-
+# groups = fetch_20newsgroups()
+#
+# data_train = fetch_20newsgroups(subset='train', random_state=21)
+# train_label = data_train.target
+# data_test = fetch_20newsgroups(subset='test', random_state=21)
+# test_label = data_test.target
+# len(data_train.data), len(data_test.data), len(test_label)
+#
+# np.unique(test_label)
+df_news = pd.DataFrame()
+for file in os.listdir(path):
+    tag = file
+    for doc in os.listdir(path+'/'+file):
+        docpath = path+'/'+file+'/'+doc
+        f = open(docpath, "r",encoding='cp1252')
+        content = f.read()
+        temp = pd.DataFrame(
+            {
+                'content':content,
+                'tag':tag
+            },index=[0]
+        )
+        df_news = pd.concat([df_news,temp])
 
 all_names = names.words()
 WNL = WordNetLemmatizer()
+
+
+
 def clean(data):
     cleaned = defaultdict(list)
     count = 0
@@ -32,10 +51,9 @@ def clean(data):
         count +=1
     return(list(cleaned.values()))
 
+x_train = clean(df_news['content'])
 
-x_train = clean(data_train.data)
-print(x_train[0])
-
+print(x_train)
 
 
 tf = TfidfVectorizer(stop_words='english', max_features=1000)
